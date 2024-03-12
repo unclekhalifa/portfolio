71c1fb96def2208d824712ed96287535e6b9552e
paste <147841049+pastc@users.noreply.github.com>
2024-03-13 00:32:33 +0900

    Finish building navbar. is lit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,134 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div data-theme="sunset" class="bg-base-200">
+    <div class="mx-auto flex min-h-screen max-w-[872px] flex-col gap-9 px-10 py-8 text-base-content md:gap-20 md:py-16">
+      <header class="z-40 flex w-full flex-col gap-2 md:flex-row md:justify-between">
+        <span class="text-lg">Tahir Murata</span>
+        <div className="text-sm custom-breadcrumbs">
+          <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/posts">Posts</router-link></li>
+          </ul>
+        </div>
+      </header>
+      <div
+        class="fixed z-0 h-[134px] w-[134px] lg:w-[300px] lg:h-[300px] rounded-full bg-primary blur-[150px] md:blur-[350px] opacity-50 left-0 top-0">
+      </div>
+      <div
+        class="fixed z-0 h-[134px] w-[134px] lg:w-[300px] lg:h-[300px] rounded-full bg-secondary blur-[150px] md:blur-[350px] opacity-50 right-0 bottom-0">
+      </div>
+      <router-view></router-view>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.custom-breadcrumbs {
+  max-width: 100%;
+  overflow-x: auto;
+  padding-top: 0.5%;
 }
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.custom-breadcrumbs>ul {
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+  min-height: min-content;
 }
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.custom-breadcrumbs>ul {
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+  min-height: min-content;
+}
+
+.custom-breadcrumbs>ul>li {
+  display: flex;
+  align-items: center;
+}
+
+.custom-breadcrumbs>ul>li {
+  display: flex;
+  align-items: center;
+}
+
+.custom-breadcrumbs>ul>li>a {
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+}
+
+.custom-breadcrumbs>ul>li>a {
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+}
+
+@media (hover:hover) {
+
+  .custom-breadcrumbs>ul>li>a:hover {
+    text-decoration-line: underline;
+  }
+}
+
+@media (hover:hover) {
+
+  .custom-breadcrumbs>ul>li>a:hover {
+    text-decoration-line: underline;
+  }
+}
+
+.custom-breadcrumbs>ul>li>a:focus {
+  outline: 2px solid transparent;
+  outline-offset: 2px;
+}
+
+.custom-breadcrumbs>ul>li>a:focus {
+  outline: 2px solid transparent;
+  outline-offset: 2px;
+}
+
+.custom-breadcrumbs>ul>li>a:focus-visible {
+  outline: 2px solid currentColor;
+  outline-offset: 2px;
+}
+
+.custom-breadcrumbs>ul>li>a:focus-visible {
+  outline: 2px solid currentColor;
+  outline-offset: 2px;
+}
+
+.custom-breadcrumbs>ul>li+*:before {
+  content: "/";
+  margin-left: 0.5rem
+    /* 8px */
+  ;
+  margin-right: 0.75rem
+    /* 12px */
+  ;
+  display: block;
+  opacity: 0.4;
+  background-color: transparent;
+}
+
+.custom-breadcrumbs>ul>li+*:before {
+  content: "/";
+  margin-left: 0.5rem
+    /* 8px */
+  ;
+  margin-right: 0.75rem
+    /* 12px */
+  ;
+  display: block;
+  opacity: 0.4;
+  background-color: transparent;
+}
+
+[dir="rtl"] .custom-breadcrumbs>ul>li+*:before {
+  --tw-rotate: -135deg;
+}
+
+[dir="rtl"] .custom-breadcrumbs>ul>li+*:before {
+  --tw-rotate: -135deg;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from "vue";
 import "./style.css";
+import { createRouter, createWebHashHistory } from "vue-router";
+import "./style.css";
+import HelloWorld from "./components/HelloWorld.vue";
 import App from "./App.vue";
+import Home from "./components/Home.vue";
 
-createApp(App).mount("#app");
+const routes = [
+  { path: "/", component: Home },
+  { path: "/hello", component: HelloWorld },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
