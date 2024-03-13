f9649d199faed7aa1644311f01cc033a7cf976c4
paste <147841049+pastc@users.noreply.github.com>
2024-03-13 12:16:59 +0900

    Finish homepage

---
+++ src/components/Box.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { IconArrowUpRight } from '@tabler/icons-vue'
+
+defineProps<{ title: string, subTitle?: string, content?: string, url: string, tags?: string[] }>()
+</script>
+
+<template>
+  <router-link v-if="!url.includes('http')"
+    class="group flex cursor-pointer flex-col gap-2 rounded-md border border-base-content p-4 transition-all duration-300 hover:-translate-y-2"
+    v-bind:to="url">
+    <div class="flex w-full justify-between gap-2 flex-row items-center">
+      <p class="text-base-content font-semibold">{{ title }}</p>
+      <div class="flex flex-row items-center gap-4">
+        <p class="text-neutral-content">{{ subTitle }}</p>
+        <IconArrowUpRight color="#94a0a9" class="transition-all duration-300 group-hover:translate-x-1" size=22 />
+      </div>
+    </div>
+    <p class="truncate text-neutral-content">{{ content }}</p>
+  </router-link>
+  <a v-if="url.includes('http')" :href="url" rel="noreferrer" target="_blank"
+    class="group flex cursor-pointer flex-col gap-2 rounded-md border border-base-content p-4 transition-all duration-300 hover:-translate-y-2">
+    <div class="flex w-full justify-between gap-2 flex-row items-center">
+      <p class="text-base-content font-semibold">{{ title }}</p>
+      <div class="flex flex-row items-center gap-4">
+        <p class="text-neutral-content">{{ subTitle }}</p>
+        <IconArrowUpRight color="#94a0a9" class="transition-all duration-300 group-hover:translate-x-1" size=22 />
+      </div>
+    </div>
+    <div class="pt-1 flex flex-row flex-wrap gap-2">
+      <div v-for="k in tags" className="badge badge-neutral">
+        {{ k }}
+      </div>
+    </div>
+
+  </a>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,162 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import Box from '../components/Box.vue';
+
+const years = computed(() => {
+  const start = new Date('2022-10-01');
+  const now = new Date();
+  return now.getFullYear() - start.getFullYear();
+});
+</script>
+
+<template>
+  <div class="flex flex-col gap-20">
+    <article class="flex flex-col gap-8 md:flex-row-reverse md:justify-end md:gap-12">
+      <div class="flex flex-col gap-8">
+        <h1 class="text-3xl">Hi, I’m Tahir</h1>
+        <h2 class="w-auto max-w-[80%] leading-6">Hello, I am a
+          <strong class="font-normal text-accent">Japanese backend developer</strong>
+          with over
+          <strong class="font-normal text-accent">{{ years }} year</strong>
+          of web experience. I am currently working with
+          <strong class="font-normal text-accent">Golang and Typescript</strong>
+          . Outside of programming I like to gamble and take photographs.
+        </h2>
+        <div class="custom-breadcrumbs">
+          <ul>
+            <li>
+              <a href="https://github.com/pastc" rel="noreferrer" target="_blank">
+                Github
+              </a>
+            </li>
+            <li>
+              <a href="https://gitlab.com/paste" rel="noreferrer" target="_blank">
+                Gitlab
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </article>
+
+    <article class="flex flex-col gap-8">
+      <header class="flex w-full flex-row justify-between gap-2">
+        <h3 class="text-lg">Latest posts</h3>
+        <router-link to="/posts" class="text-neutral-content text-sm underline decoration-dashed underline-offset-8"
+          rel="noreferrer">See all posts
+        </router-link>
+      </header>
+      <section class="grid grid-cols-1 gap-4 sm:grid-cols-2 mx-auto">
+        <Box title="Lorem Ipsum" subTitle="Cicero"
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fringilla risus vel velit egestas, eleifend volutpat risus semper. Aenean eu nulla ut augue euismod ornare vel a urna."
+          url="404" />
+        <Box title="Lorem Ipsum" subTitle="Cicero"
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fringilla risus vel velit egestas, eleifend volutpat risus semper. Aenean eu nulla ut augue euismod ornare vel a urna."
+          url="404" />
+        <Box title="Lorem Ipsum" subTitle="Cicero"
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fringilla risus vel velit egestas, eleifend volutpat risus semper. Aenean eu nulla ut augue euismod ornare vel a urna."
+          url="404" />
+        <Box title="Lorem Ipsum" subTitle="Cicero"
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fringilla risus vel velit egestas, eleifend volutpat risus semper. Aenean eu nulla ut augue euismod ornare vel a urna."
+          url="404" />
+      </section>
+    </article>
+
+    <article class="flex flex-col gap-8">
+      <header class="flex w-full flex-row justify-between gap-2">
+        <h3 class="text-lg">Featured projects</h3>
+        <router-link to="/projects" class="text-neutral-content text-sm underline decoration-dashed underline-offset-8"
+          target="_self" rel="noreferrer">See all projects
+        </router-link>
+      </header>
+      <section class="grid grid-cols-1 gap-4 sm:grid-cols-2 justify-center items-stretch">
+        <Box title="Vero" url="https://github.com/pastc/vero"
+          :tags="['golang', 'algorithm', 'provably-fair', 'hash']" />
+        <Box title="Petch" url="https://github.com/pastc/petch" :tags="['golang', 'screenfetch']" />
+      </section>
+    </article>
+
+    <article class="flex flex-col gap-8">
+      <header class="flex w-full flex-row justify-between gap-2">
+        <h3 class="text-lg">Get in touch</h3>
+      </header>
+      <section class="flex flex-col gap-6 text-sm">
+        <h1 class="text-neutral-content">Email me at contact@tahir.sh</h1>
+        <div class="text-neutral-content custom-breadcrumbs">
+          <ul>
+            <li>
+              <a href="https://github.com/pastc" rel="noreferrer" target="_blank">
+                Github
+              </a>
+            </li>
+            <li>
+              <a href="https://gitlab.com/paste" rel="noreferrer" target="_blank">
+                Gitlab
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </article>
+  </div>
+</template>
+
+<style scoped>
+.custom-breadcrumbs {
+  max-width: 100%;
+  overflow-x: auto;
+  padding-top: 0.5%;
+}
+
+.custom-breadcrumbs>ul {
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+  min-height: min-content;
+}
+
+.custom-breadcrumbs>ul>li {
+  display: flex;
+  align-items: center;
+}
+
+.custom-breadcrumbs>ul>li>a {
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+}
+
+@media (hover:hover) {
+
+  .custom-breadcrumbs>ul>li>a:hover {
+    text-decoration-line: underline;
+  }
+}
+
+.custom-breadcrumbs>ul>li>a:focus {
+  outline: 2px solid transparent;
+  outline-offset: 2px;
+}
+
+.custom-breadcrumbs>ul>li>a:focus-visible {
+  outline: 2px solid currentColor;
+  outline-offset: 2px;
+}
+
+.custom-breadcrumbs>ul>li+*:before {
+  content: "/";
+  margin-left: 0.5rem
+    /* 8px */
+  ;
+  margin-right: 0.75rem
+    /* 12px */
+  ;
+  display: block;
+  opacity: 0.4;
+  background-color: transparent;
+}
+
+[dir="rtl"] .custom-breadcrumbs>ul>li+*:before {
+  --tw-rotate: -180deg;
+}
+</style>
