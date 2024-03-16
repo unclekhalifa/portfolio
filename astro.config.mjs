b2f698251f43b86c20c9f7288d9ad4e53462ea49
paste <147841049+pastc@users.noreply.github.com>
2024-03-16 13:23:28 +0900

    Optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   integrations: [tailwind()],
   site: "https://tahir.sh",
 });
