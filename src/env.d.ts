23a84b330501bda15bf9c18bb1666d01f5b75c6a
Khalifa Idris <ibrahim.idris.developer@gmail.com>
2024-03-31 09:07:44 +0100

    Cleanup secrets and refactor public firebase configs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
+
+interface ImportMetaEnv {
+  readonly SANITY_PROJECT_ID: string
+  readonly SANITY_DATASET: string
+  readonly SANITY_STUDIO_BASE_PATH: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
---
+++ src/firebase/client.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA-723kdVz8kQFqE4MmVAGXSnDnD8yhog0',
+  authDomain: 'my-portfolio-709c7.firebaseapp.com',
+  projectId: 'my-portfolio-709c7',
+  storageBucket: 'my-portfolio-709c7.appspot.com',
+  messagingSenderId: '1073353935403',
+  appId: '1:1073353935403:web:53be3a0e6e8b287bbc386e',
+  measurementId: 'G-RRQ245ERE5',
+}
+
+export const app = initializeApp(firebaseConfig)
